14741619e081847a599f0a4b08b2c6a0ef1b2b61
Egor.Kraev <egor.kraev@transferwise.com>
2024-06-06 10:35:44 +0100

    Cleanup and nicer examples for classifier scores

--- wise_topic/__init__.py
+++ wise_topic/__init__.py
@@ -1,3 +1,5 @@
 from wise_topic.topic.greedy import greedy_topic_tree, tree_summary
-from wise_topic.classifier.classifier import llm_classifier
-from wise_topic.classifier.prompts import binary_prompt
+from wise_topic.classifier.classifier import (
+    llm_classifier_binary,
+    llm_classifier_multiple,
+)
--- wise_topic/classifier/classifier.py
+++ wise_topic/classifier/classifier.py
@@ -6,8 +6,55 @@ from langchain_core.messages import HumanMessage
 from langchain_core.prompts import PromptTemplate
 from langchain_core.language_models import BaseChatModel
 
+from wise_topic.classifier.prompts import binary_prompt, multi_choice_prompt
 
-def llm_classifier(llm: BaseChatModel, messages, valid_options, top_logprobs=5, max_tokens=1):
+
+def llm_classifier_binary(llm: BaseChatModel, question: str):
+    prompt_value = (
+        PromptTemplate.from_template(binary_prompt)
+        .invoke({"question": question})
+        .to_messages()
+    )
+    out = llm_classifier(llm, [prompt_value], ["0", "1"])
+    return {False: out[0], True: out[1]}
+
+
+def llm_classifier_multiple(
+    llm: BaseChatModel,
+    question: str,
+    answer_options: List[str],
+    include_other: bool = False,
+):
+
+    assert (
+        len(answer_options) <= 9
+    ), "Only up to 9 answer options are supported at the moment"
+    categories = "\n".join([f"{i + 1}. {t}" for i, t in enumerate(answer_options)])
+    prompt_value = (
+        PromptTemplate.from_template(multi_choice_prompt(include_other))
+        .invoke({"question": question, "categories": categories})
+        .to_messages()
+    )
+
+    valid_outputs = [str(i) for i in range(len(answer_options) + 1)]
+    scores = llm_classifier(
+        llm,
+        [prompt_value],
+        valid_outputs if include_other else valid_outputs[1:],
+        top_logprobs=15,
+    )
+    if include_other:
+        used_options = ["Other"] + list(answer_options)
+    else:
+        used_options = list(answer_options)
+
+    out = {k: v for k, v in zip(used_options, scores)}
+    return out
+
+
+def llm_classifier(
+    llm: BaseChatModel, messages, valid_options, top_logprobs=5, max_tokens=1
+) -> np.ndarray:
     result = llm.generate(
         messages,
         logprobs=True,
@@ -22,12 +69,12 @@ def llm_classifier(llm: BaseChatModel, messages, valid_options, top_logprobs=5, 
     return scores
 
 
-def logprobs_to_scores(logprobs, valid_options: List[str]):
+def logprobs_to_scores(logprobs, valid_options: List[str]) -> np.ndarray:
     scores = np.array(len(valid_options) * [float("-inf")])
     matches = False
     for i, c in enumerate(valid_options):
         for p in logprobs:
-            if isinstance(p, dict): # Langchain interface
+            if isinstance(p, dict):  # Langchain interface
                 token = p["token"]
                 logprob = p["logprob"]
             else:  # OpenAI interface
